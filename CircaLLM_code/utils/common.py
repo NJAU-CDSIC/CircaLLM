c1fa681ec3949508e51ef80833a699ab43c04503
NJAU-CDSIC <142595806+NJAU-CDSIC@users.noreply.github.com>
2025-06-16 11:14:17 +0800

    Delete CircaLLM_code directory

--- CircaLLM_code/utils/common.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class TASKS:
-    RECONSTRUCTION: str = "reconstruction"
-    CLASSIFICATION: str = "classification"
-    DIFFRHYTHM: str = "diffrhythm"
-    EMBED: str = "embedding"
